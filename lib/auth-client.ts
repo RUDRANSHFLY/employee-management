b8581eb8b8f9cbf2169360e130a3e5d5b463e080
RUDRANSH BARADIYA <mayamayavir@gmail.com>
2026-07-05 11:43:29 +0530

    chore(auth): integrate infer additional fields plugin for enhanced authentication

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,8 @@
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import type { auth } from "@/auth/auth";
+
 export const { signIn, signUp, useSession, signOut } = createAuthClient({
+  plugins: [inferAdditionalFields<typeof auth>()],
   baseURL: "http://localhost:3000",
 });
