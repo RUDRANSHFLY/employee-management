adad127f06a3dbf8d4971e0cdb79fa0df6de4358
RUDRANSH BARADIYA <mayamayavir@gmail.com>
2026-07-05 16:19:44 +0530

    feat(home-page): implement authentication check and redirect to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Employee Management</h1>
-    </div>
-  );
+import { auth } from "@/auth/auth";
+import { dashboardPathForRole } from "@/lib/roles";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  redirect(dashboardPathForRole(session.user?.role));
 }
