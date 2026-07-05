4e793c045af20cb11cae434eae95e42a915159ba
RUDRANSH BARADIYA <mayamayavir@gmail.com>
2026-07-05 17:00:24 +0530

    feat(auth-client): add get session method to auth client exports

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { auth } from "@/auth/auth";
 
-export const { signIn, signUp, useSession, signOut } = createAuthClient({
+export const { signIn, signUp, useSession, signOut, getSession } = createAuthClient({
   plugins: [inferAdditionalFields<typeof auth>()],
   baseURL: "http://localhost:3000",
 });
